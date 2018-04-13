14f9b437f108151f8794bdb007ca518a69661e92
UncleRus <unclerus@gmail.com>
2018-04-13 18:42:33 +0500

    Example for ultrasonic range meters

---
+++ examples/ultrasonic/main/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_system.h>
+#include <ultrasonic.h>
+
+#define MAX_DISTANCE_CM 500 // 5m max
+#define TRIGGER_GPIO 17
+#define ECHO_GPIO 16
+
+void ultrasonic_test(void *pvParamters)
+{
+    ultrasonic_sensor_t sensor = {
+        .trigger_pin = TRIGGER_GPIO,
+        .echo_pin = ECHO_GPIO
+    };
+
+    ultrasonic_init(&sensor);
+
+    while (true)
+    {
+        uint32_t distance;
+        esp_err_t res = ultrasonic_measure_cm(&sensor, MAX_DISTANCE_CM, &distance);
+        if (res != ESP_OK)
+        {
+            printf("Error: ");
+            switch (res)
+            {
+                case ESP_ERR_ULTRASONIC_PING:
+                    printf("Cannot ping (device is in invalid state)\n");
+                    break;
+                case ESP_ERR_ULTRASONIC_PING_TIMEOUT:
+                    printf("Ping timeout (no device found)\n");
+                    break;
+                case ESP_ERR_ULTRASONIC_ECHO_TIMEOUT:
+                    printf("Echo timeout (i.e. distance too big)\n");
+                    break;
+                default:
+                    printf("%d\n", res);
+            }
+        }
+        else
+            printf("Distance: %d cm, %.02f m\n", distance, distance / 100.0);
+
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
+}
+
+void app_main()
+{
+    xTaskCreate(ultrasonic_test, "ultrasonic_test", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
+}
+
