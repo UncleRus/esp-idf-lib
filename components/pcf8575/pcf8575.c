064a08559c5a5bd52829166563452f0e30da95c4
UncleRus <unclerus@gmail.com>
2019-08-03 17:19:16 +0500

    Driver for PCF8575

---
+++ components/pcf8575/pcf8575.c
@@ -0,0 +1,75 @@
+/**
+ * @file pcf8575.c
+ *
+ * ESP-IDF driver for PCF8575 remote 16-bit I/O expander for I2C-bus
+ *
+ * Copyright (C) 2019 Ruslan V. Uss <https://github.com/UncleRus>
+ *
+ * MIT Licensed as described in the file LICENSE
+ */
+#include "pcf8575.h"
+#include <esp_err.h>
+
+#define I2C_FREQ_HZ 400000
+
+#define CHECK(x) do { esp_err_t __; if ((__ = x) != ESP_OK) return __; } while (0)
+#define CHECK_ARG(VAL) do { if (!(VAL)) return ESP_ERR_INVALID_ARG; } while (0)
+#define BV(x) (1 << (x))
+
+static esp_err_t read_port(i2c_dev_t *dev, uint16_t *val)
+{
+    CHECK_ARG(dev);
+    CHECK_ARG(val);
+
+    I2C_DEV_TAKE_MUTEX(dev);
+    I2C_DEV_CHECK(dev, i2c_dev_read(dev, NULL, 0, val, 2));
+    I2C_DEV_GIVE_MUTEX(dev);
+
+    return ESP_OK;
+}
+
+static esp_err_t write_port(i2c_dev_t *dev, uint16_t val)
+{
+    CHECK_ARG(dev);
+
+    I2C_DEV_TAKE_MUTEX(dev);
+    I2C_DEV_CHECK(dev, i2c_dev_write(dev, NULL, 0, &val, 2));
+    I2C_DEV_GIVE_MUTEX(dev);
+
+    return ESP_OK;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+esp_err_t pcf8575_init_desc(i2c_dev_t *dev, i2c_port_t port, uint8_t addr, gpio_num_t sda_gpio, gpio_num_t scl_gpio)
+{
+    CHECK_ARG(dev);
+    CHECK_ARG(addr & 0x20);
+
+    dev->port = port;
+    dev->addr = addr;
+    dev->cfg.sda_io_num = sda_gpio;
+    dev->cfg.scl_io_num = scl_gpio;
+    dev->cfg.master.clk_speed = I2C_FREQ_HZ;
+
+    CHECK(i2c_dev_create_mutex(dev));
+
+    return ESP_OK;
+}
+
+esp_err_t pcf8575_free_desc(i2c_dev_t *dev)
+{
+    CHECK_ARG(dev);
+
+    return i2c_dev_delete_mutex(dev);
+}
+
+esp_err_t pcf8575_port_read(i2c_dev_t *dev, uint16_t *val)
+{
+    return read_port(dev, val);
+}
+
+esp_err_t pcf8575_port_write(i2c_dev_t *dev, uint16_t val)
+{
+    return write_port(dev, val);
+}
---
+++ components/pcf8575/pcf8575.h
@@ -0,0 +1,67 @@
+/**
+ * @file pcf8575.h
+ * @defgroup pcf8575 pcf8575
+ * @{
+ *
+ * ESP-IDF driver for PCF8575 remote 16-bit I/O expander for I2C-bus
+ *
+ * Copyright (C) 2019 Ruslan V. Uss <https://github.com/UncleRus>
+ *
+ * MIT Licensed as described in the file LICENSE
+ */
+#ifndef __PCF8575_H__
+#define __PCF8575_H__
+
+#include <stddef.h>
+#include <i2cdev.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PCF8575_I2C_ADDR_BASE 0x20
+
+/**
+ * @brief Initialize device descriptior
+ *
+ * SCL frequency is 400kHz
+ *
+ * @param dev Pointer to I2C device descriptor
+ * @param port I2C port number
+ * @param addr I2C address (`0b0100<A2><A1><A0>` for PCF8575)
+ * @param sda_gpio SDA GPIO
+ * @param scl_gpio SCL GPIO
+ * @return `ESP_OK` on success
+ */
+esp_err_t pcf8575_init_desc(i2c_dev_t *dev, i2c_port_t port, uint8_t addr, gpio_num_t sda_gpio, gpio_num_t scl_gpio);
+
+/**
+ * @brief Free device descriptor
+ * @param dev Pointer to I2C device descriptor
+ * @return `ESP_OK` on success
+ */
+esp_err_t pcf8575_free_desc(i2c_dev_t *dev);
+
+/**
+ * @brief Read GPIO port value
+ * @param dev Pointer to I2C device descriptor
+ * @param val 8-bit GPIO port value
+ * @return `ESP_OK` on success
+ */
+esp_err_t pcf8575_port_read(i2c_dev_t *dev, uint16_t *val);
+
+/**
+ * @brief Write value to GPIO port
+ * @param dev Pointer to I2C device descriptor
+ * @param value GPIO port value
+ * @return ESP_OK on success
+ */
+esp_err_t pcf8575_port_write(i2c_dev_t *dev, uint16_t value);
+
+#ifdef __cplusplus
+}
+#endif
+
+/**@}*/
+
+#endif /* __PCF8575_H__ */
