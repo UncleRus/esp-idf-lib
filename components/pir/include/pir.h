1786c8c678bfa93b134e98a3dd9b77691ff381e9
Tomoyuki Sakurai <y@trombik.org>
2020-09-02 16:13:24 +0900

    doc: update license headers

--- components/pir/include/pir.h
+++ components/pir/include/pir.h
@@ -2,6 +2,7 @@
  * MIT License
  *
  * Copyright (c) 2019 Nocluna
+ *                    Tomoyuki Sakurai
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
