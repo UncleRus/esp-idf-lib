2f2f70cbfad58257758331c0542bce58c001775b
Tomoyuki Sakurai <y@trombik.org>
2020-09-02 10:14:52 +0900

    bugfix: remove ESP32-specific functions, mjd_* functions
    * reame mjd_hcsr501 to pir
    * fixup `include`s
    * style

--- examples/pir/main/hcsr501_pir_sensor_main.c
+++ examples/pir/main/hcsr501_pir_sensor_main.c
@@ -1,5 +1,13 @@
-#include "mjd.h"
-#include "mjd_hcsr501.h"
+#include <FreeRTOS.h>
+#include <freertos/semphr.h>
+#include <freertos/task.h>
+#include <driver/gpio.h>
+#include <esp_log.h>
+#include <esp_idf_lib_helpers.h>
+#include <pir.h>
+
+#define RTOS_DELAY_1SEC (1000 / portTICK_PERIOD_MS)
+#define RTOS_TASK_PRIORITY_NORMAL (5)
 
 /*
  * Logging
@@ -20,26 +28,6 @@ static const int MY_SENSOR_DATA_GPIO_NUM = CONFIG_MY_SENSOR_DATA_GPIO_NUM;
 #define MYAPP_RTOS_TASK_STACK_SIZE_8K (8192)
 #define MYAPP_RTOS_TASK_PRIORITY_NORMAL (RTOS_TASK_PRIORITY_NORMAL)
 
-/*
- * GPIOMUX sensor data pin -> on-board LED
- *
- * Use the MUX to route the GPIO input pin receiving the signal from the sensor to the GPIO Output pin of the on-board LED.
- *
- * Only the loopback signals 224-228 can be configured to be routed from one input GPIO directly to another output GPIO.
- *
- * KConfig wiring types:
- *   + Adafruit Huzzah32: 1 MY_LED_ON_DEVBOARD_WIRING_TYPE_LED_LOW_SIDE
- *   + LOLIN D32:         2 MY_LED_ON_DEVBOARD_WIRING_TYPE_LED_HIGH_SIDE
- *
- */
-void gpio_setup_gpiomux_sensor_to_led() {
-    ESP_LOGD(TAG, "%s()", __FUNCTION__);
-
-    bool invert_signal = MY_LED_ON_DEVBOARD_WIRING_TYPE == 2;
-    gpio_matrix_in(MY_SENSOR_DATA_GPIO_NUM, SIG_IN_FUNC224_IDX, invert_signal);
-    gpio_matrix_out(MY_LED_ON_DEVBOARD_GPIO_NUM, SIG_IN_FUNC224_IDX, false, false);
-}
-
 /**
  * TASKS
  */
@@ -52,8 +40,6 @@ void gpio_setup_gpiomux_sensor_to_led() {
 
 void sensor_task(void *pvParameter) {
     ESP_LOGI(TAG, "%s()", __FUNCTION__);
-
-    mjd_log_memory_statistics();
 
     uint32_t total_nbr_of_detections = 0;
 
@@ -69,16 +55,6 @@ void sensor_task(void *pvParameter) {
     ESP_LOGI(TAG, "  MY_LED_ON_DEVBOARD_GPIO_NUM:    %i", MY_LED_ON_DEVBOARD_GPIO_NUM);
     ESP_LOGI(TAG, "  MY_LED_ON_DEVBOARD_WIRING_TYPE: %i", MY_LED_ON_DEVBOARD_WIRING_TYPE);
 
-    mjd_log_memory_statistics();
-
-    mjd_led_config_t led_config =
-        { 0 };
-    led_config.gpio_num = MY_LED_ON_DEVBOARD_GPIO_NUM;
-    led_config.wiring_type = MY_LED_ON_DEVBOARD_WIRING_TYPE; // 1 GND MCU Huzzah32 | 2 VCC MCU Lolin32lite
-    mjd_led_config(&led_config);
-
-    gpio_setup_gpiomux_sensor_to_led();
-
     /********************************************************************************
      * SENSOR
      *
@@ -93,12 +69,12 @@ void sensor_task(void *pvParameter) {
     ESP_LOGW(TAG, "  @important     + False positives might occur during calibration.");
 
     // Init sensor
-    mjd_hcsr501_config_t hcsr501_config = MJD_HCSR501_CONFIG_DEFAULT();
+    pir_config_t hcsr501_config = MJD_HCSR501_CONFIG_DEFAULT();
     hcsr501_config.data_gpio_num = MY_SENSOR_DATA_GPIO_NUM;
 
-    f_retval = mjd_hcsr501_init(&hcsr501_config);
+    f_retval = pir_init(&hcsr501_config);
     if (f_retval != ESP_OK) {
-        ESP_LOGE(TAG, "mjd_hcsr501_init() err %i %s", f_retval, esp_err_to_name(f_retval));
+        ESP_LOGE(TAG, "pir_init() err %i %s", f_retval, esp_err_to_name(f_retval));
         // GOTO
         goto cleanup;
     }
@@ -127,8 +103,6 @@ void sensor_task(void *pvParameter) {
 void other_task(void *pvParameter) {
     ESP_LOGI(TAG, "%s()", __FUNCTION__);
 
-    mjd_log_memory_statistics();
-
     while (true) {
         ESP_LOGI(TAG, "Task other_task: every 1 second - INPUT GPIO#%i: Actual value=%i", MY_SENSOR_DATA_GPIO_NUM,
                 gpio_get_level(MY_SENSOR_DATA_GPIO_NUM));
@@ -144,15 +118,9 @@ void app_main() {
 
     BaseType_t xReturned;
 
-    mjd_log_memory_statistics();
-
     /********************************************************************************
      * MY STANDARD Init
      */
-    mjd_log_wakeup_details();
-    mjd_log_chip_info();
-    mjd_log_time();
-    mjd_log_memory_statistics();
     ESP_LOGI(TAG, "@doc Wait X seconds after power-on (start logic analyzer, let sensors become active!)");
     vTaskDelay(RTOS_DELAY_1SEC);
 
@@ -162,9 +130,13 @@ void app_main() {
      * @important For stability (RMT + Wifi + BT): always use xTaskCreatePinnedToCore(APP_CPU_NUM) [Opposed to xTaskCreate()]
      *
      */
-    xReturned = xTaskCreatePinnedToCore(&sensor_task, "sensor_task (name)", MYAPP_RTOS_TASK_STACK_SIZE_8K, NULL,
-    MYAPP_RTOS_TASK_PRIORITY_NORMAL, NULL,
-    APP_CPU_NUM);
+    xReturned = xTaskCreatePinnedToCore(&sensor_task,
+                                        "sensor_task (name)",
+                                        MYAPP_RTOS_TASK_STACK_SIZE_8K,
+                                        NULL,
+                                        MYAPP_RTOS_TASK_PRIORITY_NORMAL,
+                                        NULL,
+                                        APP_CPU_NUM);
     if (xReturned == pdPASS) {
         ESP_LOGI(TAG, "OK Task sensor_task has been created, and is running right now");
     }
@@ -176,9 +148,13 @@ void app_main() {
      *
      * @important For stability (RMT + Wifi + BT): always use xTaskCreatePinnedToCore(APP_CPU_NUM) [Opposed to xTaskCreate()]
      */
-    xReturned = xTaskCreatePinnedToCore(&other_task, "othertask (name)", MYAPP_RTOS_TASK_STACK_SIZE_8K, NULL,
-    MYAPP_RTOS_TASK_PRIORITY_NORMAL, NULL,
-    APP_CPU_NUM);
+    xReturned = xTaskCreatePinnedToCore(&other_task,
+                                        "othertask (name)",
+                                        MYAPP_RTOS_TASK_STACK_SIZE_8K,
+                                        NULL,
+                                        MYAPP_RTOS_TASK_PRIORITY_NORMAL,
+                                        NULL,
+                                        APP_CPU_NUM);
     if (xReturned == pdPASS) {
         ESP_LOGI(TAG, "OK Task other_task has been created, and is running right now");
     }
