a8c475014931b0851e7b00c454cd069a120f678e
Mark Buckaway <mark@buckaway.ca>
2020-10-23 09:20:39 -0400

    ESP32S2 support

--- components/ds1302/ds1302.c
+++ components/ds1302/ds1302.c
@@ -17,6 +17,8 @@
 
 #if HELPER_TARGET_VERSION == HELPER_TARGET_VERSION_ESP32_V4
 #include <esp32/rom/ets_sys.h>
+#elif HELPER_TARGET_VERSION == HELPER_TARGET_VERSION_ESP32S2_V4
+#include <esp32s2/rom/ets_sys.h>
 #else
 #include <rom/ets_sys.h>
 #endif
