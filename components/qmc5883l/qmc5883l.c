dd17522e23672933da666b00ea6317fd9ac66262
UncleRus <unclerus@gmail.com>
2020-12-16 01:15:55 +0500

    qmc5883l: Fix possible race condition

--- components/qmc5883l/qmc5883l.c
+++ components/qmc5883l/qmc5883l.c
@@ -36,28 +36,36 @@ static const char *TAG = "QMC5883L";
 #define CHECK(x) do { esp_err_t __; if ((__ = x) != ESP_OK) return __; } while (0)
 #define CHECK_ARG(VAL) do { if (!(VAL)) return ESP_ERR_INVALID_ARG; } while (0)
 
-static esp_err_t write_register(qmc5883l_t *dev, uint8_t reg, uint8_t val)
+inline static esp_err_t write_reg_nolock(qmc5883l_t *dev, uint8_t reg, uint8_t val)
+{
+    return i2c_dev_write_reg(&dev->i2c_dev, reg, &val, 1);
+}
+
+inline static esp_err_t read_reg_nolock(qmc5883l_t *dev, uint8_t reg, uint8_t *val)
+{
+    return i2c_dev_read_reg(&dev->i2c_dev, reg, val, 1);
+}
+
+static esp_err_t write_reg(qmc5883l_t *dev, uint8_t reg, uint8_t val)
 {
     CHECK_ARG(dev);
 
     I2C_DEV_TAKE_MUTEX(&dev->i2c_dev);
-    esp_err_t ret = i2c_dev_write_reg(&dev->i2c_dev, reg, &val, 1);
-    if (ret != ESP_OK)
-        ESP_LOGE(TAG, "Could not write 0x%02x to register 0x%02x, err = %d", val, reg, ret);
+    I2C_DEV_CHECK(&dev->i2c_dev, write_reg_nolock(dev, reg, val));
     I2C_DEV_GIVE_MUTEX(&dev->i2c_dev);
-    return ret;
+
+    return ESP_OK;
 }
 
-static esp_err_t read_register(qmc5883l_t *dev, uint8_t reg, uint8_t *val)
+static esp_err_t read_reg(qmc5883l_t *dev, uint8_t reg, uint8_t *val)
 {
     CHECK_ARG(dev && val);
 
     I2C_DEV_TAKE_MUTEX(&dev->i2c_dev);
-    esp_err_t ret = i2c_dev_read_reg(&dev->i2c_dev, reg, val, 1);
-    if (ret != ESP_OK)
-        ESP_LOGE(TAG, "Could not read register 0x%02x, err = %d", reg, ret);
+    I2C_DEV_CHECK(&dev->i2c_dev, read_reg_nolock(dev, reg, val));
     I2C_DEV_GIVE_MUTEX(&dev->i2c_dev);
-    return ret;
+
+    return ESP_OK;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -85,7 +93,7 @@ esp_err_t qmc5883l_free_desc(qmc5883l_t *dev)
 
 esp_err_t qmc5883l_reset(qmc5883l_t *dev)
 {
-    CHECK(write_register(dev, REG_CTRL2, 0x80));
+    CHECK(write_reg(dev, REG_CTRL2, 0x80));
     dev->range = QMC5883L_RNG_2;
 
     return ESP_OK;
@@ -93,14 +101,21 @@ esp_err_t qmc5883l_reset(qmc5883l_t *dev)
 
 esp_err_t qmc5883l_get_chip_id(qmc5883l_t *dev, uint8_t *id)
 {
-    return read_register(dev, REG_ID, id);
+    return read_reg(dev, REG_ID, id);
 }
 
 esp_err_t qmc5883l_set_mode(qmc5883l_t *dev, qmc5883l_mode_t mode)
 {
+    CHECK_ARG(dev && mode <= QMC5883L_MODE_CONTINUOUS);
+
     uint8_t v;
-    CHECK(read_register(dev, REG_CTRL1, &v));
-    return write_register(dev, REG_CTRL1, (v & 0xfe) | (mode & 1));
+
+    I2C_DEV_TAKE_MUTEX(&dev->i2c_dev);
+    I2C_DEV_CHECK(&dev->i2c_dev, read_reg_nolock(dev, REG_CTRL1, &v));
+    I2C_DEV_CHECK(&dev->i2c_dev, write_reg_nolock(dev, REG_CTRL1, (v & 0xfe) | mode));
+    I2C_DEV_GIVE_MUTEX(&dev->i2c_dev);
+
+    return ESP_OK;
 }
 
 esp_err_t qmc5883l_get_mode(qmc5883l_t *dev, qmc5883l_mode_t *mode)
@@ -108,7 +123,7 @@ esp_err_t qmc5883l_get_mode(qmc5883l_t *dev, qmc5883l_mode_t *mode)
     CHECK_ARG(mode);
 
     uint8_t v;
-    CHECK(read_register(dev, REG_CTRL1, &v));
+    CHECK(read_reg(dev, REG_CTRL1, &v));
     *mode = v & 1;
 
     return ESP_OK;
@@ -116,11 +131,18 @@ esp_err_t qmc5883l_get_mode(qmc5883l_t *dev, qmc5883l_mode_t *mode)
 
 esp_err_t qmc5883l_set_config(qmc5883l_t *dev, qmc5883l_odr_t odr, qmc5883l_osr_t osr, qmc5883l_range_t rng)
 {
+    CHECK_ARG(dev && odr <= QMC5883L_DR_200 && osr <= QMC5883L_OSR_512 && rng <= QMC5883L_RNG_8);
+
     uint8_t v;
-    CHECK(read_register(dev, REG_CTRL1, &v));
+    I2C_DEV_TAKE_MUTEX(&dev->i2c_dev);
+    I2C_DEV_CHECK(&dev->i2c_dev, read_reg_nolock(dev, REG_CTRL1, &v));
     dev->range = rng;
-    CHECK(write_register(dev, REG_FBR, 1)); // Define set/reset period
-    return write_register(dev, REG_CTRL1, (v & 0x03) | ((odr & 3) << 2) | ((rng & 1) << 4) | ((osr & 3) << 6));
+    I2C_DEV_CHECK(&dev->i2c_dev, write_reg_nolock(dev, REG_FBR, 1)); // Define set/reset period
+    I2C_DEV_CHECK(&dev->i2c_dev, write_reg_nolock(dev, REG_CTRL1,
+            (v & 0x03) | ((odr & 3) << 2) | ((rng & 1) << 4) | ((osr & 3) << 6)));
+    I2C_DEV_GIVE_MUTEX(&dev->i2c_dev);
+
+    return ESP_OK;
 }
 
 esp_err_t qmc5883l_get_config(qmc5883l_t *dev, qmc5883l_odr_t *odr, qmc5883l_osr_t *osr, qmc5883l_range_t *rng)
@@ -128,7 +150,7 @@ esp_err_t qmc5883l_get_config(qmc5883l_t *dev, qmc5883l_odr_t *odr, qmc5883l_osr
     CHECK_ARG(odr && osr && rng);
 
     uint8_t v;
-    CHECK(read_register(dev, REG_CTRL1, &v));
+    CHECK(read_reg(dev, REG_CTRL1, &v));
     *odr = (v >> 2) & 3;
     *osr = (v >> 6) & 3;
     *rng = (v >> 4) & 1;
@@ -138,7 +160,7 @@ esp_err_t qmc5883l_get_config(qmc5883l_t *dev, qmc5883l_odr_t *odr, qmc5883l_osr
 
 esp_err_t qmc5883l_set_int(qmc5883l_t *dev, bool enable)
 {
-    return write_register(dev, REG_CTRL2, enable ? 1 : 0);
+    return write_reg(dev, REG_CTRL2, enable ? 1 : 0);
 }
 
 esp_err_t qmc5883l_get_int(qmc5883l_t *dev, bool *enable)
@@ -146,7 +168,7 @@ esp_err_t qmc5883l_get_int(qmc5883l_t *dev, bool *enable)
     CHECK_ARG(enable);
 
     uint8_t v;
-    CHECK(read_register(dev, REG_CTRL2, &v));
+    CHECK(read_reg(dev, REG_CTRL2, &v));
     *enable = v & 1;
 
     return ESP_OK;
@@ -157,7 +179,7 @@ esp_err_t qmc5883l_data_ready(qmc5883l_t *dev, bool *ready)
     CHECK_ARG(ready);
 
     uint8_t v;
-    CHECK(read_register(dev, REG_STATE, &v));
+    CHECK(read_reg(dev, REG_STATE, &v));
     *ready = v & 1;
 
     return ESP_OK;
