55efaeb767a34ebddbc7a36c8ab61327fb1ecd04
Tomoyuki Sakurai <y@trombik.org>
2021-11-13 02:10:08 +0700

    meta: lookup Person in persons.yml

--- spec/person.rb
+++ spec/person.rb
@@ -4,13 +4,38 @@ require "yaml"
 
 # A class that repesents Peson
 class Person
+  PERSON_FILE = File.expand_path(File.join(File.dirname("__FILE__"), "persons.yml")).freeze
+
   def initialize(hash)
-    raise ArgumentError, "Person must be a hash, but got `#{hash.class}`" unless hash.kind_of?(Hash)
-    @metadata = hash
-    raise ArgumentError, "name is missing: hash: #{hash.inspect}" unless @metadata.key?("name")
+    raise ArgumentError, "Person must be a hash, but got `#{hash.class}`" unless hash.is_a?(Hash)
+    raise ArgumentError, "name is missing: hash: #{hash.inspect}" unless hash.key?("name")
+
+    @metadata = lookup_person(hash["name"])
   end
 
-  attr_reader :metadata
+  attr_reader :metadata, :persons
+
+  def load_person_file
+    File.read(PERSON_FILE)
+  end
+
+  def parse(string)
+    return @persons if @persons
+
+    @persons = YAML.safe_load(string)
+  rescue StandardError => e
+    warn "failed to parse #{PERSON_FILE} as YAML"
+    raise e
+  end
+
+  def lookup_person(name)
+    parse(load_person_file)
+    person = persons.select { |p| p["name"] == name }
+    raise ArgumentError, "cannot find Person with name `#{name}` in #{PERSON_FILE}" unless person
+    raise ArgumentError, "Person with name `#{name}` has duplicated entry in #{PERSON_FILE}" if person.length > 1
+
+    person.first
+  end
 
   def full_name?
     metadata.key?("full_name")
