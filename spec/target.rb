44dcc629bbab171a1ba942ec063aafb8442105a2
Tomoyuki Sakurai <y@trombik.org>
2021-11-13 01:09:30 +0700

    meta: fix Target resource in the class
    raise error when Strng is given

--- spec/target.rb
+++ spec/target.rb
@@ -5,10 +5,11 @@ require "yaml"
 # A class that represents build target
 
 class Target
-  def initialize(name)
-    raise ArgumentError, "name is missing" unless name
+  def initialize(hash)
+    raise ArgumentError, "expects Hash, but got `#{hash.class}`" unless hash.kind_of?(Hash)
+    raise ArgumentError, "name is missing" unless hash.key?("name")
 
-    @name = name
+    @name = hash["name"]
   end
 
   attr_reader :name
