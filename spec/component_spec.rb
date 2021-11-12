c305a098850d8b147b4e3d40ff1bda60f083d62c
Tomoyuki Sakurai <y@trombik.org>
2021-11-12 17:16:21 +0700

    meta: add tests for Component objects

--- spec/component_spec.rb
+++ spec/component_spec.rb
@@ -2,6 +2,7 @@
 
 require_relative "spec_helper"
 
+# rubocop:disable Metrics/BlockLength
 components.each do |c|
   RSpec.describe c.to_s do
     subject { c }
@@ -23,4 +24,49 @@ components.each do |c|
       end
     end
   end
+
+  RSpec.describe c.to_s do
+    subject { Component.new(c.path) }
+
+    it "does not raise error" do
+      expect { subject }.not_to raise_error
+    end
+
+    it "has components" do
+      expect(subject.components?).to be true
+    end
+
+    it "has one or more of components" do
+      expect(subject.components.length).to be > 0
+    end
+
+    it "has name" do
+      expect(subject.name?).to be true
+    end
+
+    it "has String name" do
+      expect(subject.name).to be_kino_of(String)
+    end
+
+    it "has non-empty name" do
+      expect(subject.name).not_to be_empty
+    end
+
+    it "has description" do
+      expect(subject.description?).to be true
+    end
+
+    it "has String description" do
+      expect(subject.description).to be_kino_of(String)
+    end
+
+    it "has a primary group" do
+      expect(subject.group?).to be true
+    end
+
+    it "has a valid primary group" do
+      expect { subject.group }.not_to raise_error
+    end
+  end
 end
+# rubocop:enable Metrics/BlockLength
