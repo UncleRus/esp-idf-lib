790f64fd27cace1ad0de0536e977ab2afd798d3e
Tomoyuki Sakurai <y@trombik.org>
2021-11-14 00:44:50 +0700

    meta: support String shorthand for `Target`

--- spec/target.rb
+++ spec/target.rb
@@ -7,18 +7,29 @@ require "yaml"
 class Target
   VALID_KEYS = %w[name].freeze
 
-  def initialize(hash)
-    raise ArgumentError, "expects Hash, but got `#{hash.class}`" unless hash.is_a?(Hash)
-    validate_keys(hash)
-
-    @metadata = hash
+  def initialize(arg)
+    validate_arg(arg)
+    validate_keys(arg) if arg.is_a?(Hash)
+    @metadata = if arg.is_a?(String)
+                  { "name" => arg }
+                else
+                  arg
+                end
   end
 
   attr_reader :metadata
 
+  def valid_arg_class?(arg)
+    arg.is_a?(String) || arg.is_a?(Hash)
+  end
+
+  def validate_arg(arg)
+    raise ArgumentError, "String or dict is ecpected, but got `#{arg.class}`" unless valid_arg_class?(arg)
+  end
+
   def validate_keys(hash)
     hash.each_key do |k|
-      raise ArgumentError, "unknown key: `#{k}`. valid keys are: #{VALID_KEYS.join(' ')}" unless VALID_KEYS.include? k
+      raise ArgumentError, "unknown key: `#{k}`. valid keys are: #{VALID_KEYS.join(' ')}" unless VALID_KEYS.include?(k)
     end
   end
 
