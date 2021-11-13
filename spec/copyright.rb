df67fbe9fac06fcdb7c179b7185dc95512cf759a
Tomoyuki Sakurai <y@trombik.org>
2021-11-13 12:52:57 +0700

    meta: add validate_keys to Copyright

--- spec/copyright.rb
+++ spec/copyright.rb
@@ -3,14 +3,23 @@
 require_relative "person"
 
 class Copyright
+  VALID_KEYS = %w[name year]
+
   def initialize(hash)
     raise ArgumentError, "expect Hash, got `#{hash.class}`" unless hash.is_a?(Hash)
     raise ArgumentError, "missing key `name`" unless hash.key?("name")
 
+    validate_keys(hash)
     @metadata = hash
   end
 
   attr_reader :metadata
+
+  def validate_keys(hash)
+    hash.each_key do |k|
+      raise ArgumentError, "unknown key: `#{k}`. valid keys are: #{VALID_KEYS.join(' ')}" unless VALID_KEYS.include? k
+    end
+  end
 
   def name
     Person.new("name" => metadata["name"])
