c49f27f79d4c80f3d391fc1451a43b16b347bc6c
Tomoyuki Sakurai <y@trombik.org>
2021-11-13 12:36:00 +0700

    meta: fix validate_keys

--- spec/group.rb
+++ spec/group.rb
@@ -7,15 +7,16 @@ class Group
     raise ArgumentError, "group must be a hash, but got #{hash.class}" unless hash.is_a? Hash
     raise ArgumentError, "missing key `name`" unless hash.key?("name")
 
+    validate_keys(hash)
     @metadata = hash
   end
 
-
   def validate_keys(hash)
     hash.each_key do |k|
-      raise ArgumentError, "unknown key: `#{k}`. valid keys are: #{VALID_KEYS.join(' ')}"
+      raise ArgumentError, "unknown key: `#{k}`. valid keys are: #{VALID_KEYS.join(' ')}" unless VALID_KEYS.include? k
     end
   end
+
   def name?
     @metadata.key?("name")
   end
