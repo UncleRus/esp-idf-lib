5eeedc2eb3781f24c65afe9af55932360a84c3fb
Tomoyuki Sakurai <y@trombik.org>
2021-11-13 19:43:37 +0700

    meta: add tests for groups.yml

---
+++ spec/group_list.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "yaml"
+
+class GroupList
+
+  # path to `groups.yml`
+  def initialize(arg)
+    @path = File.expand_path(arg)
+  end
+
+  attr_reader :path
+
+  def load_file
+    File.read(path)
+  end
+
+  def parse
+    YAML.safe_load(load_file)
+  end
+
+  def metadata
+    @metadata if @metadata
+
+    @metadata = parse
+  end
+
+  def all
+    metadata
+  end
+
+  def lookup(name)
+    metadata.select { |g| g["name"] == name }
+  end
+end
