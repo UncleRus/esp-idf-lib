46e727aa7b05c2aff05973ca2eb082538a309929
saasaa <mail@saasaa.xyz>
2021-12-17 11:38:27 +0100

    fix: formatting according to library style

--- examples/hts221/main/main.c
+++ examples/hts221/main/main.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <esp_system.h>
-#include <hts221.h>
-#include <string.h>
-#include <esp_system.h>
 #include <esp_err.h>
 #include <esp_log.h>
+#include <esp_system.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <hts221.h>
+#include <stdio.h>
+#include <string.h>
 
 #if defined(CONFIG_IDF_TARGET_ESP8266)
 #define SDA_GPIO 2
@@ -24,31 +23,31 @@
 static const char *TAG = "hts221-example";
 void task(void *pvParameters)
 {
-	i2c_dev_t dev = { 0 };
-	ESP_ERROR_CHECK(
-		hts221_init_desc(&dev, ADDR, 0, SDA_GPIO,
-				 SCL_GPIO)); // Initialize HTS221 descriptor
-	ESP_ERROR_CHECK(hts221_setup(
-		&dev)); // Setup HTS221 to default values according to datasheet.
+    i2c_dev_t dev = {0};
+    ESP_ERROR_CHECK(hts221_init_desc(&dev, ADDR, 0, SDA_GPIO,
+                                     SCL_GPIO)); // Initialize HTS221 descriptor
+    ESP_ERROR_CHECK(hts221_setup(
+        &dev)); // Setup HTS221 to default values according to datasheet.
 
-	float temperature, humidity;
+    float temperature, humidity;
 
-	while (1) {
-		esp_err_t res = hts221_read_data(&dev, &temperature, &humidity);
-		if (res == ESP_OK)
-			ESP_LOGI(TAG, "Temperature: %.1f°C, Humidity: %.2f%%",
-				 temperature, humidity);
-		else
-			ESP_LOGE(TAG, "Error reading data: %d (%s)", res,
-				 esp_err_to_name(res));
+    while (1)
+    {
+        esp_err_t res = hts221_read_data(&dev, &temperature, &humidity);
+        if (res == ESP_OK)
+            ESP_LOGI(TAG, "Temperature: %.1f°C, Humidity: %.2f%%", temperature,
+                     humidity);
+        else
+            ESP_LOGE(TAG, "Error reading data: %d (%s)", res,
+                     esp_err_to_name(res));
 
-		vTaskDelay(pdMS_TO_TICKS(1000));
-	}
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
 
 void app_main()
 {
-	ESP_ERROR_CHECK(i2cdev_init());
-	xTaskCreatePinnedToCore(task, TAG, configMINIMAL_STACK_SIZE * 8, NULL,
-				5, NULL, APP_CPU_NUM);
+    ESP_ERROR_CHECK(i2cdev_init());
+    xTaskCreatePinnedToCore(task, TAG, configMINIMAL_STACK_SIZE * 8, NULL, 5,
+                            NULL, APP_CPU_NUM);
 }
