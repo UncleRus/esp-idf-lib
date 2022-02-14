e61e93b45f3c39e011a05c88525561b90803ea06
Tomoyuki Sakurai <y@trombik.org>
2022-02-14 08:19:33 +0700

    bugfix: include ROM header files explicitly
    ets_sys.h must be included explicitly in esp-idf v5. see:
    https://github.com/espressif/esp-idf/commit/5810ed1d043cbc54c9a7616db1e84d99f71f3ccb

--- components/hx711/hx711.c
+++ components/hx711/hx711.c
@@ -37,6 +37,19 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <esp_timer.h>
+#if CONFIG_IDF_TARGET_ESP32
+#include <esp32/rom/ets_sys.h>
+#elif CONFIG_IDF_TARGET_ESP32S2
+#include <esp32s2/rom/ets_sys.h>
+#elif CONFIG_IDF_TARGET_ESP32S3
+#include <esp32s3/rom/ets_sys.h>
+#elif CONFIG_IDF_TARGET_ESP32C3
+#include <esp32c3/rom/ets_sys.h>
+#elif CONFIG_IDF_TARGET_ESP32H2
+#include <esp32h2/rom/ets_sys.h>
+#elif CONFIG_IDF_TARGET_ESP32C2
+#include <esp32c2/rom/ets_sys.h>
+#endif
 #include <esp_idf_lib_helpers.h>
 #include "hx711.h"
 
--- components/scd30/scd30.c
+++ components/scd30/scd30.c
@@ -43,6 +43,19 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <esp_log.h>
+#if CONFIG_IDF_TARGET_ESP32
+#include <esp32/rom/ets_sys.h>
+#elif CONFIG_IDF_TARGET_ESP32S2
+#include <esp32s2/rom/ets_sys.h>
+#elif CONFIG_IDF_TARGET_ESP32S3
+#include <esp32s3/rom/ets_sys.h>
+#elif CONFIG_IDF_TARGET_ESP32C3
+#include <esp32c3/rom/ets_sys.h>
+#elif CONFIG_IDF_TARGET_ESP32H2
+#include <esp32h2/rom/ets_sys.h>
+#elif CONFIG_IDF_TARGET_ESP32C2
+#include <esp32c2/rom/ets_sys.h>
+#endif
 #include <esp_idf_lib_helpers.h>
 #include "scd30.h"
 
