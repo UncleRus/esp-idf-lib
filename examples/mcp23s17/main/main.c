3527851872028ff402931e4025b6123e68bf6f2d
UncleRus <unclerus@gmail.com>
2022-03-06 14:08:08 +0500

    mcp23x17: move GPIO definitions to Kconfig.projbuild in msp23s17 example

--- examples/mcp23s17/main/main.c
+++ examples/mcp23s17/main/main.c
@@ -1,50 +1,47 @@
-#include <stdio.h>
+#include <esp_log.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <mcp23x17.h>
 #include <driver/gpio.h>
 #include <string.h>
 
-#define INTA_GPIO 19  // INTA pin
+#define HOST SPI2_HOST
 
-#define PIN_NUM_MOSI 23
-#define PIN_NUM_MISO 19
-#define PIN_NUM_CLK  18
-#define PIN_NUM_CS   5
+static const char *TAG = "mcp23s17 example";
 
-#define HOST VSPI_HOST
+static mcp23x17_t dev = { 0 };
 
 static void intr_handler(void *arg)
 {
-    printf("Interrupt!\n");
+    uint32_t v;
+    mcp23x17_get_level(&dev, 0, &v);
+    ESP_LOGI(TAG, "Interrupt! PORTA0: %d\n", v);
 }
 
 void test(void *pvParameters)
 {
-    mcp23x17_t dev;
-
     // Configure SPI bus
     spi_bus_config_t cfg = {
-       .mosi_io_num = PIN_NUM_MOSI,
-       .miso_io_num = PIN_NUM_MISO,
-       .sclk_io_num = PIN_NUM_CLK,
+       .mosi_io_num = CONFIG_EXAMPLE_MOSI_GPIO,
+       .miso_io_num = CONFIG_EXAMPLE_MISO_GPIO,
+       .sclk_io_num = CONFIG_EXAMPLE_SCLK_GPIO,
        .quadwp_io_num = -1,
        .quadhd_io_num = -1,
        .max_transfer_sz = 0,
        .flags = 0
     };
     ESP_ERROR_CHECK(spi_bus_initialize(HOST, &cfg, 1));
-    ESP_ERROR_CHECK(mcp23x17_init_desc_spi(&dev, HOST, MCP23X17_MAX_SPI_FREQ, MCP23X17_ADDR_BASE, PIN_NUM_CS));
+    ESP_ERROR_CHECK(mcp23x17_init_desc_spi(&dev, HOST, MCP23X17_MAX_SPI_FREQ, CONFIG_EXAMPLE_ADDRESS, CONFIG_EXAMPLE_CS_GPIO));
 
     // Setup PORTA0 as input
     mcp23x17_set_mode(&dev, 0, MCP23X17_GPIO_INPUT);
     // Setup interrupt on it
     mcp23x17_set_interrupt(&dev, 0, MCP23X17_INT_ANY_EDGE);
 
-    gpio_set_direction(INTA_GPIO, GPIO_MODE_INPUT);
-    gpio_set_intr_type(INTA_GPIO, GPIO_INTR_ANYEDGE);
+    gpio_set_direction(CONFIG_EXAMPLE_INTA_GPIO, GPIO_MODE_INPUT);
+    gpio_set_intr_type(CONFIG_EXAMPLE_INTA_GPIO, GPIO_INTR_ANYEDGE);
     gpio_install_isr_service(0);
-    gpio_isr_handler_add(INTA_GPIO, intr_handler, NULL);
+    gpio_isr_handler_add(CONFIG_EXAMPLE_INTA_GPIO, intr_handler, NULL);
 
     // Setup PORTB0 as output
     mcp23x17_set_mode(&dev, 8, MCP23X17_GPIO_OUTPUT);
