e7b5f0af6b659af459e470b39ad9447391826f8d
UncleRus <unclerus@gmail.com>
2022-05-21 15:30:14 +0500

    pca9557: add pin-based functions to set mode, support for TCA9534

--- examples/pca9557/main/main.c
+++ examples/pca9557/main/main.c
@@ -3,17 +3,17 @@
 #include <freertos/task.h>
 #include <pca9557.h>
 #include <driver/gpio.h>
-#include <string.h>
 
 void test(void *pvParameters)
 {
-    i2c_dev_t dev;
-    memset(&dev, 0, sizeof(i2c_dev_t));
+    i2c_dev_t dev = { 0 };
 
     ESP_ERROR_CHECK(pca9557_init_desc(&dev, CONFIG_EXAMPLE_I2C_ADDR, 0, CONFIG_EXAMPLE_I2C_MASTER_SDA, CONFIG_EXAMPLE_I2C_MASTER_SCL));
 
-    // Setup IO0 as input, IO1-IO7 as outputs
-    ESP_ERROR_CHECK(pca9557_port_set_mode(&dev, 0x01));
+    // Setup IO0 as input, IO3 as output
+    // instead of calling pca9557_set_mode() multiple times, you can call pca9557_port_set_mode()
+    ESP_ERROR_CHECK(pca9557_set_mode(&dev, 0, PCA9557_MODE_INPUT));
+    ESP_ERROR_CHECK(pca9557_set_mode(&dev, 3, PCA9557_MODE_OUTPUT));
 
     // blink on IO3 and read IO0
     bool on = true;
@@ -22,7 +22,7 @@ void test(void *pvParameters)
         ESP_ERROR_CHECK(pca9557_set_level(&dev, 3, on));
         on = !on;
         uint8_t in = 0;
-        ESP_ERROR_CHECK(pca9557_port_read(&dev, &in));
+        ESP_ERROR_CHECK(pca9557_port_read(&dev, &in)); // you can use pca9557_get_level() instead
         printf("IO0: %s\n", (in & 1) ? "high" : "low");
         vTaskDelay(pdMS_TO_TICKS(500));
     }
