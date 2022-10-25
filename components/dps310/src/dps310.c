604f74cd09c18568536e3257476ec63134ccdc07
Tomoyuki Sakurai <y@trombik.org>
2022-10-25 17:53:22 +0700

    bugfix: include freertos/task.h, fixes build on ESP8266

--- components/dps310/src/dps310.c
+++ components/dps310/src/dps310.c
@@ -25,6 +25,8 @@
 #include <inttypes.h>
 
 /* esp-idf headers */
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 #include <esp_log.h>
 #include <esp_idf_lib_helpers.h>
 #include <esp_err.h>
