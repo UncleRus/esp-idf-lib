2fa66d032bcc04485fbb169ab227160f027c10f4
UncleRus <unclerus@gmail.com>
2023-07-28 12:22:45 +0500

    fix: fix includes for esp8266 in calibration

--- components/calibration/calibration.c
+++ components/calibration/calibration.c
@@ -37,6 +37,7 @@
 #include "calibration.h"
 #include <esp_log.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define CHECK(x) do { esp_err_t __; if ((__ = x) != ESP_OK) return __; } while (0)
 #define CHECK_ARG(VAL) do { if (!(VAL)) return ESP_ERR_INVALID_ARG; } while (0)
