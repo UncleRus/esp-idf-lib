93ec3a5d11f6338b3cc27f3b530189b7996ca754
UncleRus <unclerus@gmail.com>
2023-08-18 15:12:35 +0500

    feat: support ds1822 and max31850 in ds18x20, refactoring, fix example readme

--- examples/ds18x20/single/main/main.c
+++ examples/ds18x20/single/main/main.c
@@ -9,7 +9,7 @@ static const gpio_num_t SENSOR_GPIO = CONFIG_EXAMPLE_ONEWIRE_GPIO;
 
 // Use address of your own sensor here!
 // You can find out the address of your sensor by running ds18x20_multi example
-static const ds18x20_addr_t SENSOR_ADDR = CONFIG_EXAMPLE_DS18X20_ADDR;
+static const onewire_addr_t SENSOR_ADDR = CONFIG_EXAMPLE_DS18X20_ADDR;
 
 static const char *TAG = "ds18x20_test";
 
