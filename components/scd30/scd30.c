64d3cca3be3a23956b7cea55aed2c6513fe2ac72
Axel Lin <axel.lin@ingics.com>
2023-09-27 16:59:04 +0800

    scd30: Fix build with esp32c6
    Also make it sort with proper older for esp32c*.
    
    Signed-off-by: Axel Lin <axel.lin@ingics.com>

--- components/scd30/scd30.c
+++ components/scd30/scd30.c
@@ -49,12 +49,14 @@
 #include <esp32s2/rom/ets_sys.h>
 #elif CONFIG_IDF_TARGET_ESP32S3
 #include <esp32s3/rom/ets_sys.h>
-#elif CONFIG_IDF_TARGET_ESP32C3
-#include <esp32c3/rom/ets_sys.h>
-#elif CONFIG_IDF_TARGET_ESP32H2
-#include <esp32h2/rom/ets_sys.h>
 #elif CONFIG_IDF_TARGET_ESP32C2
 #include <esp32c2/rom/ets_sys.h>
+#elif CONFIG_IDF_TARGET_ESP32C3
+#include <esp32c3/rom/ets_sys.h>
+#elif CONFIG_IDF_TARGET_ESP32C6
+#include <esp32c6/rom/ets_sys.h>
+#elif CONFIG_IDF_TARGET_ESP32H2
+#include <esp32h2/rom/ets_sys.h>
 #endif
 #include <esp_idf_lib_helpers.h>
 #include "scd30.h"
